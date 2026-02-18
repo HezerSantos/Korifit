c5cf5fb1a6291dc3a36fbffb84f0428fe84cefa5
Hezer Santos <HezerSantos24@gmail.com>
2026-02-18 17:46:46 -0600

    Progress on csrf controller

---
+++ controllers/csrf_controller.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"Korifit/helpers"
+	"crypto/rand"
+	"encoding/hex"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	r "math/rand"
+	"time"
+)
+
+var CSRF_SECRET = []byte(os.Getenv("CSRF_SECRET"))
+
+func GetCsrfToken(c *gin.Context) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+
+	if err != nil {
+		helpers.NetworkError(c, err)
+		c.Abort()
+		return
+	}
+
+	hexStr := hex.EncodeToString(b)
+	r.Seed(time.Now().UnixNano())
+
+	n := r.Intn(10)
+
+	claims := jwt.MapClaims{
+		"csrfToken": hexStr,
+		"key":       n,
+		"exp":       time.Now().Add(time.Duration(15) * time.Minute).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signed, err := token.SignedString(CSRF_SECRET)
+}
